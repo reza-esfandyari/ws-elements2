3c952023ded843c1b35488ffacf7571d9a89e948
REZA-DESKTOP\Reza <m.r.esfandyari.work@gmail.com>
2016-10-20 14:58:51 -0600

    sudo change

--- src/assets/deploymentScripts/StartServices.sh
+++ src/assets/deploymentScripts/StartServices.sh
@@ -2,6 +2,6 @@
 #Deployment script to start services
 #Reza Esfandyari 10/20/2016
 echo Starting services
-mkdir StartingServices
-pm2 start http-server 
+sudo mkdir StartingServices
+sudo pm2 start http-server 
 echo Finishing up starting services 
