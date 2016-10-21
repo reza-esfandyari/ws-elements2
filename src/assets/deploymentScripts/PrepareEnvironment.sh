534b9d3b332261638d34795215947f3cbd83b281
REZA-DESKTOP\Reza <m.r.esfandyari.work@gmail.com>
2016-10-21 17:16:45 -0600

    ws with pm2 log

--- src/assets/deploymentScripts/PrepareEnvironment.sh
+++ src/assets/deploymentScripts/PrepareEnvironment.sh
@@ -2,7 +2,7 @@
 #Deployment script used to prepare the environment
 #Reza Esfandyari 10/16/2016
 echo Starting prepare the environment by stopping and deleting existing running services 
-sudo  pm2 stop all
-sudo  pm2 delete all
-sudo  pm2 kill
+pm2 list
+pm2 kill
+pm2 list
 echo Finish up environment preparation
