2bbae4573b919361aa8fb4fd331d9402cda5989b
REZA-DESKTOP\Reza <m.r.esfandyari.work@gmail.com>
2016-10-22 14:13:48 -0600

    ws hellooooooooo

--- src/assets/deploymentScripts/PrepareEnvironment.sh
+++ src/assets/deploymentScripts/PrepareEnvironment.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #Deployment script used to prepare the environment
 #Reza Esfandyari 10/16/2016
-echo Starting prepare the environment by stopping and deleting existing running services 
-pm2 list
+echo Starting prepare the environment by stopping and deleting existing running services
+pm2 stop all
 pm2 kill
-pm2 list
 echo Finish up environment preparation
--- src/assets/deploymentScripts/StartServices.sh
+++ src/assets/deploymentScripts/StartServices.sh
@@ -2,6 +2,5 @@
 #Deployment script to start services
 #Reza Esfandyari 10/20/2016
 echo Starting services
-mkdir RezaTestEnvironment
-pm2 start http-server cd ../..
-echo Finishing up starting services 
+pm2 start http-server -- deployment-archive
+echo Finishing up starting services
