7650bda45f05af484c4de9d81ed5799784fda87d
REZA-DESKTOP\Reza <m.r.esfandyari.work@gmail.com>
2016-10-22 14:30:43 -0600

    ws hello 2

--- src/assets/deploymentScripts/PrepareEnvironment.sh
+++ src/assets/deploymentScripts/PrepareEnvironment.sh
@@ -2,6 +2,5 @@
 #Deployment script used to prepare the environment
 #Reza Esfandyari 10/16/2016
 echo Starting prepare the environment by stopping and deleting existing running services
-pm2 stop all
-pm2 kill
+sudo pm2 delete 0
 echo Finish up environment preparation
