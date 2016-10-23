b1deb494e7607149bcf0b6808fd6d626a9eabe5b
REZA-DESKTOP\Reza <m.r.esfandyari.work@gmail.com>
2016-10-22 18:54:53 -0600

    ws - final

--- src/assets/deploymentScripts/PrepareEnvironment.sh
+++ src/assets/deploymentScripts/PrepareEnvironment.sh
@@ -2,5 +2,22 @@
 #Deployment script used to prepare the environment
 #Reza Esfandyari 10/16/2016
 echo Starting prepare the environment by stopping and deleting existing running services
-sudo pm2 delete 0
+echo Listing the current directory:
+pwd
+echo Listing the ls:
+ls -la
+echo listing port before clean up
+sudo netstat -ntlp | grep LISTEN
+echo listing process before stoping
+sudo pm2 list
+echo STOPING the process
+sudo pm2 stop http-server
+echo DELETE the process
+sudo pm2 delete http-server
+echo killing all process
+sudo pm2 kill
+echo Showing the the process after deletion
+sudo pm2 list
+echo listing port after clean up
+sudo netstat -ntlp | grep LISTEN
 echo Finish up environment preparation
--- src/assets/deploymentScripts/StartServices.sh
+++ src/assets/deploymentScripts/StartServices.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #Deployment script to start services
 #Reza Esfandyari 10/20/2016
-echo Starting services
-pm2 start http-server -- deployment-archive
+echo Starting service
+echo Listing the current directory:
+pwd
+echo Listing the ls:
+ls -la
+echo listing port before staring
+sudo netstat -ntlp | grep LISTEN
+sudo pm2 start http-server -- /home/ubuntu/www/app
+echo listing port afetr starting
+sudo netstat -ntlp | grep LISTEN
+sudo pm2 list
 echo Finishing up starting services
